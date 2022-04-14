b4e11efeb1676e34758cf02e26032f8e05f43c74
YunaSong97 <unme97@naver.com>
2022-04-14 15:01:28 +0900

    refactor directories

--- BOJ/[1541] 잃어버린 괄호/Main_BOJ_1541.java
+++ BOJ/[1541] 잃어버린 괄호/Main_BOJ_1541.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main_BOJ_1541 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
