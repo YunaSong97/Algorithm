1e781d36235e7b5bf632473f7a78c49854e976bc
YunaSong97 <unme97@naver.com>
2022-04-29 15:06:47 +0900

    Sorting

--- Programmers/[42747] H-Index/PG_42747.java
+++ Programmers/[42747] H-Index/PG_42747.java
@@ -1,29 +1,19 @@
+import java.util.Arrays;
+
 public class PG_42747 {
     class Solution {
         public int solution(int[] citations) {
             int answer = 0;
-            int cnt = 0;
-            int cnt2 = 0;
 
+            Arrays.sort(citations);
             int n = citations.length;
-            for (int h = n; h >= 0; h--) {
-                for (int citation : citations) {
-                    if (citation > h) {
-                        cnt++;
-                    } else if (citation < h) {
-                        cnt2++;
-                    } else {
-                        cnt++;
-                        cnt2++;
-                    }
+
+            for (int i = 0; i < n; i++) {
+                if (citations[i] >= n - i) {
+                    answer = Math.max(n - i, answer);
                 }
-                if (cnt>=h && cnt2<=h) {
-                    answer = h;
-                    return answer;
-                }
-                cnt = 0;
-                cnt2 = 0;
             }
+
             return answer;
         }
     }
